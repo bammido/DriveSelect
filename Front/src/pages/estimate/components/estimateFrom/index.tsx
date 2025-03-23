2124490fc2dc1ecf18dcf00aa5a9050e5ce402d8
bryanfernandesFrwk <bryanfernandes@frwk.com.br>
2025-03-23 01:34:03 -0300

    docs: adicionando README

--- Front/src/pages/estimate/components/estimateFrom/index.tsx
+++ Front/src/pages/estimate/components/estimateFrom/index.tsx
@@ -1,38 +1,42 @@
-import { ErrorMessage, useFormikContext } from "formik";
-import Input from "../../../../components/input";
-import Label from "../../../../components/label/input";
-import Button from "../../../../components/button";
-import SearchDirectionBox from "../../../../components/searchDirectionBox";
+import { ErrorMessage, useFormikContext } from 'formik';
+import Input from '../../../../components/input';
+import Label from '../../../../components/label/input';
+import Button from '../../../../components/button';
+import SearchDirectionBox from '../../../../components/searchDirectionBox';
 
 export default function EstimateForm() {
+  const { handleChange, handleSubmit, setFieldValue } = useFormikContext();
 
-    const { handleChange, handleSubmit, setFieldValue } = useFormikContext()
-
-    return <div className="flex flex-col gap-8">
-        <div className="flex flex-col" >
-            <div className="flex gap-4">
-                <Label text="ID do usuário*" htmlFor="customer_id" />
-                <ErrorMessage name='customer_id' component='small' />
-            </div>
-            <Input onChange={handleChange} id="customer_id" />
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col">
+        <div className="flex gap-4">
+          <Label text="Nome*" htmlFor="customer_id" />
+          <ErrorMessage name="customer_id" component="small" />
         </div>
-        <div className="flex flex-col" >
-            <div className="flex gap-4">
-                <Label text="Endereço de origem*" htmlFor="origin" />
-                <ErrorMessage name='origin' component='small' />
-            </div>
-            <SearchDirectionBox id="origin"  onChange={value => setFieldValue('origin', value)} />
+        <Input onChange={handleChange} id="customer_id" />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex gap-4">
+          <Label text="Endereço de origem*" htmlFor="origin" />
+          <ErrorMessage name="origin" component="small" />
         </div>
-        <div className="flex flex-col" >
-             <div className="flex gap-4">
-                <Label text="Endereço de destino*" htmlFor="destination" />
-                <ErrorMessage name='destination' component='small' />
-            </div>
-            <SearchDirectionBox id="destination"  onChange={value => setFieldValue('destination', value)} />
-        </div>
-        <Button 
-            text="estimar o valor da viagem"
-            onClick={() => handleSubmit()}
+        <SearchDirectionBox
+          id="origin"
+          onChange={(value) => setFieldValue('origin', value)}
         />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex gap-4">
+          <Label text="Endereço de destino*" htmlFor="destination" />
+          <ErrorMessage name="destination" component="small" />
+        </div>
+        <SearchDirectionBox
+          id="destination"
+          onChange={(value) => setFieldValue('destination', value)}
+        />
+      </div>
+      <Button text="estimar o valor da viagem" onClick={() => handleSubmit()} />
     </div>
+  );
 }
